9ce796b74308ae3385ad6a90c4a93fed7de17ef5
James Anderson <jim.anderson@auth0.com>
2020-06-11 10:55:28 -0500

    Remove unused imports

--- mvc-login/src/main/java/com/auth0/example/SecurityConfig.java
+++ mvc-login/src/main/java/com/auth0/example/SecurityConfig.java
@@ -1,13 +1,8 @@
 package com.auth0.example;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
