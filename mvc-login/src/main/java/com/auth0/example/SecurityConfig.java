fac575ff54a4946d974f09c749577807aaa85b0e
Jim Anderson <jimranderson@gmail.com>
2023-10-23 11:47:53 -0500

    ubuntu-latest not available as machine image

--- mvc-login/src/main/java/com/auth0/example/SecurityConfig.java
+++ mvc-login/src/main/java/com/auth0/example/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.auth0.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
